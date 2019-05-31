b3e66a5d4279f63006db9203b150cfe79f0787f1
zhengfw <zfw55zfw@sina.com>
2019-05-31 15:08:52 +0800

    test git

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,11 +1,9 @@
-import java.io.Serializable;
-
 /**
  * @author zhengfawei
  * @create 2019-03-26 上午10:49
  * @desc 测试对象
  **/
-public class User implements Serializable {
+public class User  {
     private int id;
     private String name;
     private int age;
