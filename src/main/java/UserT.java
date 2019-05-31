d9571b8f0696d4d8d0979797ff4ccf4ad1e64c7e
zhengfw <zfw55zfw@sina.com>
2019-05-31 11:35:48 +0800

    test git

--- src/main/java/UserT.java
+++ src/main/java/UserT.java
@@ -1,9 +1,11 @@
+import java.io.Serializable;
+
 /**
  * @author zhengfawei
  * @create 2019-04-25 下午1:58
  * @desc
  **/
-public  class UserT extends User {
+public  class UserT extends User implements Serializable {
     private User user;
     public void dosomething(){
         System.out.println("this is erzi ");
